631b1794960571572fb72538d67cfdf64dff928f
Dominick Schroer <dominick@schroer.ca>
2022-11-06 20:53:17 -0500

    Add base scale,translate,rotate and mirror

--- modelrs/examples/variables.ex
+++ modelrs/examples/variables.ex
@@ -1,12 +1,7 @@
-var leg = cube(width=2, length=2, height=5);
-
-var table =
-    cube(width=10, length=10, height=1)
-        //->shape translate(x=10)
-        //->shape rotate(x=90)
-        //->shape scale(value=10)
-        ->left difference(right=leg)
-        ->shape chamfer(radius=0.1)
+cube(width=2, length=2, height=5)
+    ->shape translate(x=10)
+    //->shape rotate(x=90)
+    //->shape scale(size=2)
+    ->right union(left=cube(width=5, length=5, height=2))
+    ->shape fillet(radius=0.4)
     ;
-
-table;
