7f101a433f7c9f9d6a6b36ecc29a3ebdd0d3669d
Dominick Schroer <dominick@schroer.ca>
2022-11-08 15:37:23 -0500

    Fix cargo refs

--- examples/cup_holder.ex
+++ examples/cup_holder.ex
@@ -1,28 +1,39 @@
+var thickness = 5;
 
-var baseHeight = 8;
-var baseRadius = 5;
-var topHeight = 10;
-var topRadius = 8;
-
-var thickness = 1;
+var baseHeight = 55;
+var baseRadius = 73/2;
+var topHeight = 50;
+var topRadius = (90/2) + thickness;
 
 var base = cylinder(radius=baseRadius, height=baseHeight);
 var top = cylinder(radius=topRadius, height=topHeight)
             ->shape translate(z=baseHeight);
 
-var cutout = cube(width=baseRadius, height=topHeight, length=baseRadius*2)
+var cutout = cube(width=baseRadius, height=topHeight, length=topRadius)
             ->shape translate(y=-baseRadius/2)
-            ->left union(right=cylinder(radius=baseRadius/2, height=baseRadius*2)
+            ->left union(right=cylinder(radius=baseRadius/2, height=topRadius)
                 ->shape rotate(y=90))
-            ->shape translate(z=baseHeight + 6);
+            ->shape translate(z=baseHeight + (topHeight * 0.7));
 
-base
+var a = point(x=baseRadius, y=baseRadius);
+var b = point(x=baseRadius, y=topRadius);
+var c = point(x=topRadius, y=baseRadius);
+var face = line(start=a, end=b)
+    ->left join(right=line(start=b, end=c))
+    ->left join(right=line(start=a, end=c))
+    ->shape revolve(y=360)
+    ->shape rotate(x=270)
+    ->shape translate(z=(baseHeight*2)-(topRadius-baseRadius-thickness));
+
+var core = base
     ->left union(right=top)
     ->left difference(right=cylinder(radius=baseRadius - thickness, height=topHeight + baseHeight))
     ->left difference(right=cylinder(radius=topRadius - thickness, height=topHeight - thickness)
         ->shape translate(z=baseHeight + thickness))
-    ->left difference(right=cutout)
-    ->left difference(right=cutout ->shape rotate(z=90))
-    ->left difference(right=cutout ->shape rotate(z=180))
-    ->left difference(right=cutout ->shape rotate(z=270))
+    //->left difference(right=cutout)
+    //->left difference(right=cutout ->shape rotate(z=90))
+    //->left difference(right=cutout ->shape rotate(z=180))
+    //->left difference(right=cutout ->shape rotate(z=270))
 ;
+
+core ->left union(right=face);
