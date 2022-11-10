68c430249dddc503e7f24ffa4719b787d7b732d7
Dominick Schroer <dominick@schroer.ca>
2022-11-10 14:22:13 -0500

    Clean up release

--- release.sh
+++ release.sh
@@ -3,4 +3,4 @@ cargo build --release --target x86_64-unknown-linux-gnu
 cargo build --release --target x86_64-pc-windows-gnu
 
 (cd target/x86_64-unknown-linux-gnu/release/ && zip ../../../linux.zip model-script-cli)
-(cd target/x86_64-pc-windows-gnu/release/ && cp /usr/x86_64-w64-mingw32/bin/*.dll . && zip ../../../windows.zip model-script-cli.exe *.dll)
+(cd target/x86_64-pc-windows-gnu/release/ && zip ../../../windows.zip model-script-cli.exe)
