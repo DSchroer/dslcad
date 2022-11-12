4255df3a87f05aeb20b851c0a296cd7b98bdadc5
Dominick Schroer <dominick@schroer.ca>
2022-11-11 22:11:59 -0500

    Fix windows builds

--- release.sh
+++ release.sh
@@ -7,6 +7,6 @@ set -ex
 cargo build --release --target x86_64-unknown-linux-gnu
 (cd target/x86_64-unknown-linux-gnu/release/ && zip ../../../linux.zip model-script)
 
-WIN_DLLS="/usr/lib/gcc/x86_64-w64-mingw32/10-posix"
+WIN_DLLS="/usr/lib/gcc/x86_64-w64-mingw32/10-win32"
 cargo build --release --target x86_64-pc-windows-gnu
 (cd target/x86_64-pc-windows-gnu/release/ && cp $WIN_DLLS/*.dll . && zip ../../../windows.zip model-script.exe *.dll)
