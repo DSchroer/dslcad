cfccfd246940ae71b2756c0815d38276004ffcb2
Dominick Schroer <dominick@schroer.ca>
2023-02-05 21:03:51 -0700

    Add full range of explorer types

--- crates/opencascade/src/explorer.rs
+++ crates/opencascade/src/explorer.rs
@@ -1,8 +1,9 @@
 use crate::Error;
 use cxx::UniquePtr;
 use opencascade_sys::ffi::{
-    TopAbs_ShapeEnum, TopExp_Explorer, TopExp_Explorer_ctor, TopoDS_Edge, TopoDS_Shape,
-    TopoDS_cast_to_edge,
+    TopAbs_ShapeEnum, TopExp_Explorer, TopExp_Explorer_ctor, TopoDS_Edge, TopoDS_Face,
+    TopoDS_Shape, TopoDS_Vertex, TopoDS_Wire, TopoDS_cast_to_edge, TopoDS_cast_to_face,
+    TopoDS_cast_to_vertex, TopoDS_cast_to_wire,
 };
 use std::marker::PhantomData;
 
@@ -50,5 +51,41 @@ impl GeomIterator<TopoDS_Edge> for Explorer<TopoDS_Edge> {
     }
     fn current(&self) -> &TopoDS_Edge {
         TopoDS_cast_to_edge(self.explorer.Current())
+    }
+}
+
+impl GeomIterator<TopoDS_Shape> for Explorer<TopoDS_Shape> {
+    fn shape() -> TopAbs_ShapeEnum {
+        TopAbs_ShapeEnum::TopAbs_SHAPE
+    }
+    fn current(&self) -> &TopoDS_Shape {
+        self.explorer.Current()
+    }
+}
+
+impl GeomIterator<TopoDS_Face> for Explorer<TopoDS_Face> {
+    fn shape() -> TopAbs_ShapeEnum {
+        TopAbs_ShapeEnum::TopAbs_FACE
+    }
+    fn current(&self) -> &TopoDS_Face {
+        TopoDS_cast_to_face(self.explorer.Current())
+    }
+}
+
+impl GeomIterator<TopoDS_Vertex> for Explorer<TopoDS_Vertex> {
+    fn shape() -> TopAbs_ShapeEnum {
+        TopAbs_ShapeEnum::TopAbs_VERTEX
+    }
+    fn current(&self) -> &TopoDS_Vertex {
+        TopoDS_cast_to_vertex(self.explorer.Current())
+    }
+}
+
+impl GeomIterator<TopoDS_Wire> for Explorer<TopoDS_Wire> {
+    fn shape() -> TopAbs_ShapeEnum {
+        TopAbs_ShapeEnum::TopAbs_WIRE
+    }
+    fn current(&self) -> &TopoDS_Wire {
+        TopoDS_cast_to_wire(self.explorer.Current())
     }
 }
