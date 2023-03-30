02d29c003f39fdeace6d6eb99781edb855ec3c4e
Dominick Schroer <dominick@schroer.ca>
2023-03-30 10:16:35 -0600

    Restore build process

--- scripts/release.sh
+++ scripts/release.sh
@@ -2,29 +2,29 @@
 set -ex
 export CMAKE_BUILD_PARALLEL_LEVEL=12
 
-#cargo run --bin cheat_sheet > cheatsheet.md
-#(cd examples && zip -r ../examples.zip *)
-#
-## Build Linux
-#(
-#  cargo build --release --target x86_64-unknown-linux-gnu
-#  (cd target/x86_64-unknown-linux-gnu/release/ && zip ../../../linux.zip dslcad)
-#)
-#
-## Build Windows
-#(
-#  cargo build --release --target x86_64-pc-windows-gnu
-#  (cd target/x86_64-pc-windows-gnu/release/ && zip ../../../windows.zip dslcad.exe)
-#)
-#
-## Build MacOSX
-#(
-#  PATH="/osxcross/target/bin:$PATH" \
-#  CC=x86_64-apple-darwin20.4-clang \
-#  CXX=x86_64-apple-darwin20.4-clang++ \
-#  cargo build --release --target x86_64-apple-darwin
-#  (cd target/x86_64-apple-darwin/release/ && zip ../../../macosx.zip dslcad)
-#)
+cargo run --bin cheat_sheet > cheatsheet.md
+(cd examples && zip -r ../examples.zip *)
+
+# Build Linux
+(
+  cargo build --release --target x86_64-unknown-linux-gnu -vv
+  (cd target/x86_64-unknown-linux-gnu/release/ && zip ../../../linux.zip dslcad)
+)
+
+# Build Windows
+(
+  cargo build --release --target x86_64-pc-windows-gnu -vv
+  (cd target/x86_64-pc-windows-gnu/release/ && zip ../../../windows.zip dslcad.exe)
+)
+
+# Build MacOSX
+(
+  PATH="/osxcross/target/bin:$PATH" \
+  CC=x86_64-apple-darwin20.4-clang \
+  CXX=x86_64-apple-darwin20.4-clang++ \
+  cargo build --release --target x86_64-apple-darwin -vv
+  (cd target/x86_64-apple-darwin/release/ && zip ../../../macosx.zip dslcad)
+)
 
 # Build WASM
 (
