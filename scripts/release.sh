463ba06696e165ad30491c18f1a6e939cfa7a736
Dominick Schroer <dominick@schroer.ca>
2023-03-30 07:58:16 -0600

    Try wasm only

--- scripts/release.sh
+++ scripts/release.sh
@@ -1,29 +1,29 @@
 #!/usr/bin/env bash
 set -ex
 
-cargo run --bin cheat_sheet > cheatsheet.md
-(cd examples && zip -r ../examples.zip *)
-
-# Build Linux
-(
-  cargo build --release --target x86_64-unknown-linux-gnu
-  (cd target/x86_64-unknown-linux-gnu/release/ && zip ../../../linux.zip dslcad)
-)
-
-# Build Windows
-(
-  cargo build --release --target x86_64-pc-windows-gnu
-  (cd target/x86_64-pc-windows-gnu/release/ && zip ../../../windows.zip dslcad.exe)
-)
-
-# Build MacOSX
-(
-  PATH="/osxcross/target/bin:$PATH" \
-  CC=x86_64-apple-darwin20.4-clang \
-  CXX=x86_64-apple-darwin20.4-clang++ \
-  cargo build --release --target x86_64-apple-darwin
-  (cd target/x86_64-apple-darwin/release/ && zip ../../../macosx.zip dslcad)
-)
+#cargo run --bin cheat_sheet > cheatsheet.md
+#(cd examples && zip -r ../examples.zip *)
+#
+## Build Linux
+#(
+#  cargo build --release --target x86_64-unknown-linux-gnu
+#  (cd target/x86_64-unknown-linux-gnu/release/ && zip ../../../linux.zip dslcad)
+#)
+#
+## Build Windows
+#(
+#  cargo build --release --target x86_64-pc-windows-gnu
+#  (cd target/x86_64-pc-windows-gnu/release/ && zip ../../../windows.zip dslcad.exe)
+#)
+#
+## Build MacOSX
+#(
+#  PATH="/osxcross/target/bin:$PATH" \
+#  CC=x86_64-apple-darwin20.4-clang \
+#  CXX=x86_64-apple-darwin20.4-clang++ \
+#  cargo build --release --target x86_64-apple-darwin
+#  (cd target/x86_64-apple-darwin/release/ && zip ../../../macosx.zip dslcad)
+#)
 
 # Build WASM
 (
